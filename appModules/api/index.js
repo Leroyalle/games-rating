fb5a7fb19d285eaff6976596ad50d391cd99d788
Leroyalle <nikolajmelonov@yandex.ru>
2024-04-20 13:16:16 +0300

    Initial commit

---
+++ appModules/api/index.js
@@ -0,0 +1,7 @@
+const { getData, getRandomGame } = require('./api-utils');
+const endpoints = require('./config');
+module.exports = {
+  getData,
+  endpoints,
+  getRandomGame,
+};
---
+++ appModules/rating/index.js
@@ -0,0 +1,9 @@
+const config = require('./config');
+const makeRatingFile = require('./rating-file');
+const { createRating, updateRating } = require('./calculations');
+module.exports = {
+  config,
+  makeRatingFile,
+  createRating,
+  updateRating,
+};
